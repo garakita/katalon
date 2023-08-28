8bfe26f3ff376649d523f4d60e03c31687cd98f0
USER <USER@DESKTOP-67R9BDO>
2023-08-28 17:56:31 +0700

    PODCAT

---
+++ Test Suites/Test SuiteLASTTTT.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test SuiteLASTTTT</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>6fd4b807-9064-454f-9d84-5212d94bf6aa</testSuiteGuid>
+   <testCaseLink>
+      <guid>d7f0f994-4262-4b18-97b9-fd50e30c7509</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/PIOUIKJK</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
