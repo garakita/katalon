e7869066a89737d10bbb681d57444f769bacfafd
USER <USER@DESKTOP-67R9BDO>
2023-08-28 14:20:05 +0700

    TESTMERGe

--- Test Suites/Test Suite login.ts
+++ Test Suites/Test Suite login.ts
@@ -5,10 +5,17 @@
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
-   <numberOfRerun>0</numberOfRerun>
+   <numberOfRerun>3</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-   <rerunImmediately>false</rerunImmediately>
+   <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>8ace7758-f4b6-4f6a-ac99-d299db1a4915</testSuiteGuid>
+   <testCaseLink>
+      <guid>bc159077-82c3-4e3b-881c-d880ee153a0c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Case login</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
