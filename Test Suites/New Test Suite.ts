3374754700c8e2ece04b7f3c4d1e610ad7c00307
NUC <NUC@NUC13Anhi7>
2023-08-28 12:18:24 +0700

    tttt

--- Test Suites/New Test Suite.ts
+++ Test Suites/New Test Suite.ts
@@ -5,10 +5,17 @@
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
-   <numberOfRerun>0</numberOfRerun>
+   <numberOfRerun>3</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-   <rerunImmediately>false</rerunImmediately>
+   <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>3660851c-b443-44ac-9d35-f6a05cfa75a3</testSuiteGuid>
+   <testCaseLink>
+      <guid>4ea1fe3f-ae95-4cda-8d7c-3f493e030e92</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Case login</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
