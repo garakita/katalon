432fe1444e06ecc471aa3835af3ef0e5738e729a
NUC <NUC@NUC13Anhi7>
2023-08-28 19:19:03 +0700

    tttt

--- Test Suites/New Dynamic Test Suite.ts
+++ Test Suites/New Dynamic Test Suite.ts
@@ -11,5 +11,8 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>b6c6634a-3293-4586-9e18-f3a61f2a4a08</testSuiteGuid>
-   <filteringText></filteringText>
+   <filteringBuiltIn>com.kms.katalon.execution.platform.DynamicBuiltInSearch</filteringBuiltIn>
+   <filteringExtension></filteringExtension>
+   <filteringPlugin></filteringPlugin>
+   <filteringText>name=(stp) </filteringText>
 </FilteringTestSuiteEntity>
