05964e94f0050f91da37dee66b0c2c977ad85e20
USER <USER@DESKTOP-67R9BDO>
2023-08-30 09:38:55 +0700

    NATTHA9_38

--- Test Suites/New Test Suite.ts
+++ Test Suites/New Test Suite.ts
@@ -11,4 +11,11 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>53844c90-b508-4587-9b4d-fe9e757622b2</testSuiteGuid>
+   <testCaseLink>
+      <guid>969428eb-2a70-459e-94f2-e6a981e39ef3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/SPT-T120_test1</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
