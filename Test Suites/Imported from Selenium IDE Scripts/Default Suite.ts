1e92794f805d0d9b120db5673eeba4c4d98487bf
NUC <NUC@NUC13Anhi7>
2023-08-30 10:37:28 +0700

    ttt

---
+++ Test Suites/Imported from Selenium IDE Scripts/Default Suite.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Default Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>0</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>12135583-3999-4c35-98db-d332ed7e6675</testSuiteGuid>
+   <testCaseLink>
+      <guid>4f88a6e1-f616-418c-9163-89158ef592f6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Imported from Selenium IDE Scripts/Default Suite/tttt</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
