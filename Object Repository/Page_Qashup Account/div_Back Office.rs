7d3c0ae2f9fb017ecbee7d82baaccf6f5909010b
mac <ma99iimac@gmail.com>
2025-05-05 22:53:28 +0700

    odject repo update

---
+++ Object Repository/Page_Qashup Account/div_Back Office.rs
@@ -0,0 +1,94 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_Back Office</name>
+   <tag></tag>
+   <elementGuidId>ebe3831f-22ff-4681-9758-d9740c91f106</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>(.//*[normalize-space(text()) and normalize-space(.)='admin admin'])[1]/following::div[3]</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+      <webElementGuid>ccc18c26-28aa-49a2-ada3-462fab4326c1</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>w-[240px] h-[220px] sm:w-[280px] sm:h-[260px] 
+                            landscape:h-[200px] landscape:w-[200px] 
+                            landscape:md:w-[260px] landscape:md:h-[240px] 
+                            landscape:lg:w-[300px] landscape:lg:h-[280px] 
+                            lg:w-[320px] lg:h-[300px] 
+                            flex flex-col rounded-lg p-6 gap-2 xl:gap-6 sm:gap-4 bg-gradient-to-br from-[#050C14] to-[#A0A0A0] shadow-lg hover:shadow-[#A0A0A0]</value>
+      <webElementGuid>7470cded-85c0-431e-822f-93eb3e2bbd85</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Back Office</value>
+      <webElementGuid>0056c5ef-7613-40cd-a0df-e5c3f68404b8</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[@class=&quot;__className_d65c78&quot;]/div[@class=&quot;w-full&quot;]/div[@class=&quot;flex flex-col justify-center items-center min-h-[calc(100vh-64px)]&quot;]/div[@class=&quot;grid  grid-cols-1  landscape:grid-cols-3  landscape:py-4 xl:grid-cols-3  place-items-center  relative h-full  xl:gap-10  px-2 py-10  text-center text-white  md:px-4 xl:py-14 gap-4 cursor-pointer&quot;]/div[@class=&quot;w-[240px] h-[220px] sm:w-[280px] sm:h-[260px] 
+                            landscape:h-[200px] landscape:w-[200px] 
+                            landscape:md:w-[260px] landscape:md:h-[240px] 
+                            landscape:lg:w-[300px] landscape:lg:h-[280px] 
+                            lg:w-[320px] lg:h-[300px] 
+                            flex flex-col rounded-lg p-6 gap-2 xl:gap-6 sm:gap-4 bg-gradient-to-br from-[#050C14] to-[#A0A0A0] shadow-lg hover:shadow-[#A0A0A0]&quot;]</value>
+      <webElementGuid>9dad9295-4cd2-46d1-9057-80eb50882155</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='admin admin'])[1]/following::div[3]</value>
+      <webElementGuid>c70ebfc7-52cc-4d88-b483-4b2adec3b1ab</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='POS'])[1]/preceding::div[2]</value>
+      <webElementGuid>46cb806e-e6fd-43bc-9a04-39cd59da9ef1</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div/div/div/div</value>
+      <webElementGuid>2895a222-bead-4499-849a-b98e9ee92902</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//div[(text() = 'Back Office' or . = 'Back Office')]</value>
+      <webElementGuid>21fce758-af46-4190-9b6e-d649e8505a6d</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
